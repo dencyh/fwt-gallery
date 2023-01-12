871bc6ca790bf4d042a82f51cc16b25234fb14ab
Alexander Moryan <a.dencyh@gmail.com>
2023-01-12 23:24:51 +0300

    feat: add intercection observer to imgages

--- src/components/ui/gallery/galleryImage.jsx
+++ src/components/ui/gallery/galleryImage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./gallery.module.scss";
 import PropTypes from "prop-types";
+import { useInView } from "react-intersection-observer";
 
 const GalleryImage = ({ src, alt }) => {
+  const { ref, inView } = useInView({ triggerOnce: true });
   return (
-    <div className={styles.img_wrapper}>
-      {src && <img {...{ src, alt }} className={styles.img} />}
+    <div className={styles.img_wrapper} ref={ref}>
+      {src && <img src={inView ? src : ""} alt={alt} className={styles.img} />}
     </div>
   );
 };
